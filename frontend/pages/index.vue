5edf5eabf4b5a4a17c5da1ff0ced930ee263f5d0
Shashank Kanakapura Srivatsa <srivatsa@mail.uni-paderborn.de>
2021-03-15 18:23:41 +0100

    Initial layout design
    Initial layout design without working functionality

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,17 +1,88 @@
 <template>
-  <div>
-    <div>
-      Resolved:
-      <div v-for="error in resolved" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
+  <div class="grid grid-cols-1 grid-rows-1 justify-center bg-gray-100">
+    <div class="grid grid-cols-1 grid-rows-1 gap-16 min-w-0 max-w-full bg-gray-300 
+    mt-16 mx-16 px-8 py-8 rounded text-white bg-pink-600 text-3xl font-bold uppercase"> 
+    Error DashBoard
     </div>
-    <div>
-      Unresolved:
-      <div v-for="error in unresolved" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
+    <div class="grid grid-cols-1 grid-rows-1 gap-16 min-w-0 max-w-full bg-gray-300 my-4 mx-16 px-8 py-8 rounded">
+    <div class="flex flex-wrap min-w-full">
+    <div class="w-full">
+      <ul class="flex mb-0 list-none flex-wrap pt-3 flex-row">
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-md font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(1)" v-bind:class="{'text-pink-600 bg-white': openTab !== 1, 'text-white bg-pink-600': openTab === 1}">
+            <i class="fas fa-space-shuttle text-base mr-1"></i> Resolved
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-md font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(2)" v-bind:class="{'text-pink-600 bg-white': openTab !== 2, 'text-white bg-pink-600': openTab === 2}">
+            <i class="fas fa-cog text-base mr-1"></i> Unresolved
+          </a>
+        </li>
+        <li class="-mb-px mr-2 last:mr-0 flex-auto text-center">
+          <a class="text-md font-bold uppercase px-5 py-3 shadow-lg rounded block leading-normal" v-on:click="toggleTabs(3)" v-bind:class="{'text-pink-600 bg-white': openTab !== 3, 'text-white bg-pink-600': openTab === 3}">
+            <i class="fas fa-briefcase text-base"></i> Backlog
+          </a>
+        </li>
+      </ul>
+      <div class="relative grid grid-cols-1 min-w-0  bg-white w-full mb-6 mr-1 shadow-lg rounded">
+        <div class="px-4 py-5 flex-auto">
+          <div class="tab-content tab-space">
+            <div v-bind:class="{'hidden': openTab !== 1, 'block': openTab === 1}">
+              <div class="grid grid-cols-3 max-h-3 shadow-lg rounded py-6 bg-pink-400 text-white">
+                <div class="text-center font-bold">Error Code</div>
+                <div class="text-center font-bold">Error Description</div>
+                <div class="text-center font-bold">User Actions</div>
+              </div>
+              <div class="grid grid-cols-3 max-h-3 shadow-lg rounded py-6" v-for="error in resolved" :key="error.index">
+                <div class="text-center">{{ error.code }} </div>
+                <div class="text-center">{{ error.text }}</div>
+                <div class="text-center"></div>
+              </div>
+            </div>
+            <div v-bind:class="{'hidden': openTab !== 2, 'block': openTab === 2}">
+              <div class="grid grid-cols-3 max-h-3 shadow-lg rounded py-6 bg-pink-400 text-white">
+                <div class="text-center font-bold">Error Code</div>
+                <div class="text-center font-bold">Error Description</div>
+                <div class="text-center font-bold">User Actions</div>
+              </div>
+              <div class="grid grid-cols-3 gap-4 max-h-3 shadow-lg rounded py-6" v-for="error in unresolved" :key="error.index">
+                <div class="text-center">{{ error.code }} </div>
+                <div class="text-center">{{ error.text }}</div>
+                <div class="text-center"></div>
+              </div>
+            </div>
+            <div v-bind:class="{'hidden': openTab !== 3, 'block': openTab === 3}">
+              <div class="grid grid-cols-3 max-h-3 shadow-lg rounded py-6 bg-pink-400 text-white">
+                <div class="text-center font-bold">Error Code</div>
+                <div class="text-center font-bold">Error Description</div>
+                <div class="text-center font-bold">User Actions</div>
+              </div>
+              <div class="grid grid-cols-3 gap-4 max-h-3 shadow-lg rounded py-6" v-for="error in backlog" :key="error.index">
+                <div class="text-center">{{ error.code }} </div>
+                <div class="text-center">{{ error.text }}</div>
+                <div class="text-center"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <div>
-      Backlog:
-      <div v-for="error in backlog" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
+  </div>
     </div>
+    <!-- <div class="grid grid-cols-3 grid-rows-1">
+    <div class="grid max-h-8 sm:grid-cols-2 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16  overflow-auto">
+      <h1 class="">Resolved:</h1> 
+      <div class="grid max-h-3 overflow-auto" v-for="error in resolved" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
+    </div>
+    <div class="grid gap-4 max-h-8 sm:grid-cols-2 sm:px-8 sm:py-12 sm:gap-x-8 md:py-16  overflow-auto">
+      <h1 class="">Unresolved:</h1> 
+      <div class="grid gap-4 max-h-3" v-for="error in unresolved" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
+    </div>
+    <div class="grid bg-gray-400 m-16 max-h-1 overflow-auto">
+      <h1 class="">Backlog:</h1> 
+      <div class="grid gap-4 max-h-3" v-for="error in backlog" :key="error.index">`{{ error.code }}` - {{ error.text }}</div>
+    </div>
+    </div> -->
   </div>
 </template>
 
@@ -40,8 +111,15 @@ export default {
     return {
       resolved: [],
       unresolved: [],
-      backlog: []
+      backlog: [],
+      openTab: 1
     };
+  },
+  methods: {
+    toggleTabs: function(tabNumber){
+      this.openTab = tabNumber
+    }
   }
 };
 </script>
+
